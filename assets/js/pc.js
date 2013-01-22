0aa109f21a32fb6b8ae569cc643e402bf6f57a4a
Yuku Takahashi <taka84u9@gmail.com>
2013-01-23 01:53:08 +0900

    Modify pc.js to set up app and start history

--- assets/js/pc.js
+++ assets/js/pc.js
@@ -1,6 +1,22 @@
-define([],
-function () {
+define([
+  'backbone',
+  'routers/pc',
+  'collections/ContactList'
+],
+function (Backbone, Router, ContactList) {
 
   'use strict';
 
+  var app = {
+    root: '/contacts',
+    contactlist: new ContactList([
+      {name: 'Yuku Takahashi', email: 'taka84u9@gmail.com'},
+      {name: 'Yuku Takahashi', email: 'ytakahas@gmail.com'}
+    ])
+  };
+  //app.contact_list.fetch();
+
+  new Router({app: app});
+
+  Backbone.history.start({root: app.root});
 });

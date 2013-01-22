07b1d26ff9fd413dc417af92357d139a332891e9
Yuku Takahashi <taka84u9@gmail.com>
2013-01-23 01:24:14 +0900

    AppView listens route:show event and renders ShowView when it occured

--- assets/js/views/pc/AppView.js
+++ assets/js/views/pc/AppView.js
@@ -1,23 +1,37 @@
 define([
   'backbone',
   './ListView',
+  './ShowView',
   'jst'
 ],
-function (Backbone, ListView, JST) {
+function (Backbone, ListView, ShowView, JST) {
+
+  'use strict';
 
   return Backbone.View.extend({
+    mainview: null,
+
     initialize: function () {
+      this.listenTo(this.options.router, 'route:show', this.showContact);
     },
+    // View methods
+    // ------------
     render: function () {
-      this.$el.html(JST['pc/app'](this.presenter()));
+      this.$el.html(JST['pc/app']());
       this.listview = new ListView({
         collection: this.collection
       });
       this.$('#contactlist').append(this.listview.render().el);
       return this;
     },
-    presenter: function () {
-      return null;
+    // Controller methods
+    // ------------------
+    showContact: function (id) {
+      if (this.mainview) this.mainview.remove();
+      var model = this.collection.get(id);
+      if (!model) return;
+      this.mainview = new ShowView({model: model});
+      this.$('#main').append(this.mainview.render().el);
     }
   });
 });

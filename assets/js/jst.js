f30674b19730060f3e42694e29ad1adddc4ee934
Yuku Takahashi <taka84u9@gmail.com>
2013-01-24 18:50:20 +0900

    Add new button which let AppView show NewView

--- assets/js/jst.js
+++ assets/js/jst.js
@@ -1,11 +1,35 @@
-define(function(){
-
 this["JST"] = this["JST"] || {};
+
+this["JST"]["pc/_form"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<form class="form-horizontal">\n  <div class="alert alert-error alert-block" style="display: none;">\n    <ul>\n    </ul>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="name">Name</label>\n    <div class="controls">\n      <input type="text" id="name" name="name"\n        value="'+
+( source.name || '' )+
+'" placeholder="Name" required/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="email">Email</label>\n    <div class="controls">\n      <input type="text" id="email" name="email"\n        value="'+
+( source.email || '' )+
+'" placeholder="Email"\n        pattern="[^\\s@]+@\\S+\\.\\S+"/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="phone">Phone</label>\n    <div class="controls">\n      <input type="text" id="phone" name="phone"\n        value="'+
+( source.phone || '' )+
+'" placeholder="Phone"/>\n    </div>\n  </div>\n  <div class="form-actions">\n    <button class="btn btn-primary submit">Submit</button>\n    <a href="#'+
+( source.id )+
+'" class="btn cancel">Cancel</a>\n  </div>\n</form>\n';
+}
+return __p;
+};
 
 this["JST"]["pc/app"] = function(obj){
 var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
-__p+='<div id="sidebar">\n  <div id="contactlist">\n  </div>\n</div>\n<div id="main">\n</div>\n';
+__p+='<div id="sidebar">\n  <a href="#new" class="btn new">New</a>\n  <div id="contactlist">\n  </div>\n</div>\n<div id="main">\n</div>\n';
+}
+return __p;
+};
+
+this["JST"]["pc/edit"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+=''+
+( JST['pc/_form']({source: source}) )+
+'\n';
 }
 return __p;
 };
@@ -14,12 +38,34 @@ this["JST"]["pc/item"] = function(obj){
 var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
 __p+='<a href="#'+
-( id )+
-'">\n  '+
-( name )+
-'\n  '+
-( email )+
-'\n</a>\n';
+( source.id )+
+'">\n  <img src="http://www.gravatar.com/avatar/'+
+( source.hash )+
+'"/>\n  <h3>'+
+( source.name )+
+'</h3>\n  ';
+ if (source.email) { 
+;__p+='<p>'+
+( source.email )+
+'</p>';
+ } 
+;__p+='\n  ';
+ if (source.phone) { 
+;__p+='<p>'+
+( source.phone )+
+'</p>';
+ } 
+;__p+='\n</a>\n';
+}
+return __p;
+};
+
+this["JST"]["pc/new"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+=''+
+( JST['pc/_form']({source: source}) )+
+'\n';
 }
 return __p;
 };
@@ -27,20 +73,15 @@ return __p;
 this["JST"]["pc/show"] = function(obj){
 var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
 with(obj||{}){
-__p+=''+
-( name )+
-'\n'+
-( email )+
-'\n';
- if (phone) { 
-;__p+='\n  '+
-( phone )+
-'\n';
- } 
-;__p+='\n';
+__p+='<ul class="pager">\n  <li class="previous"><a href="#'+
+( source.id )+
+'/edit">Edit</a></li>\n</ul>\n<dl class="dl-horizontal">\n  <dt>Name</dt>\n  <dd>'+
+( source.name )+
+'</dd>\n  <dt>Email</dt>\n  <dd>'+
+( source.email || '' )+
+'</dd>\n  <dt>Phone</dt>\n  <dd>'+
+( source.phone || '' )+
+'</dd>\n</dl>\n';
 }
 return __p;
 };
-
-  return this["JST"];
-});
--- assets/js/views/pc/AppView.js
+++ assets/js/views/pc/AppView.js
@@ -3,18 +3,23 @@ define([
   './ListView',
   './ShowView',
   './EditView',
+  './NewView',
   'jst'
 ],
-function (Backbone, ListView, ShowView, EditView, JST) {
+function (Backbone, ListView, ShowView, EditView, NewView, JST) {
 
   'use strict';
 
   return Backbone.View.extend({
+    // The view rendered now
     mainview: null,
-
+    events: {
+      'click .new': 'navigateToNew'
+    },
     initialize: function () {
       this.listenTo(this.options.router, 'route:show', this.showContact);
       this.listenTo(this.options.router, 'route:edit', this.editContact);
+      this.listenTo(this.options.router, 'route:new', this.newContact);
     },
     // View methods
     // ------------
@@ -26,8 +31,6 @@ function (Backbone, ListView, ShowView, EditView, JST) {
       this.$('#contactlist').append(this.listview.render().el);
       return this;
     },
-    // Controller methods
-    // ------------------
     showContact: function (id) {
       if (this.mainview) this.mainview.remove();
       var model = this.collection.get(id);
@@ -45,6 +48,27 @@ function (Backbone, ListView, ShowView, EditView, JST) {
         this.options.router.navigate(this.mainview.model.id, true);
       }, this);
       this.$('#main').append(this.mainview.render().el);
+    },
+    newContact: function () {
+      if (this.mainview) this.mainview.remove();
+      var model =
+          new this.collection.model(null, {collection: this.collection});
+      this.mainview = new NewView({model: model});
+      this.mainview.on('created', function () {
+        // navigate to show page and trigger 'route:show' event
+        this.options.router.navigate(this.mainview.model.id, true);
+      }, this);
+      this.mainview.on('canceled', function () {
+        // navigate to index page and trigger 'route:index'
+        this.options.router.navigate('', true);
+      }, this);
+      this.$('#main').append(this.mainview.render().el);
+    },
+    // Controller methods
+    // ------------------
+    navigateToNew: function (e) {
+      e.preventDefault();
+      this.options.router.navigate('new', true);
     }
   });
 });

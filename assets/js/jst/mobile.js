410bd07a75d41d2c05335e4fed6240770e2fbdc9
Yuku Takahashi <taka84u9@gmail.com>
2013-01-26 14:02:06 +0900

    Add mobile/ListView and divider jst template

--- assets/js/jst/mobile.js
+++ assets/js/jst/mobile.js
@@ -1,1 +1,45 @@
 this["JST"] = this["JST"] || {};
+
+this["JST"]["divider"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<li data-role="list-divider">\n  '+
+( index )+
+'\n</li>\n';
+}
+return __p;
+};
+
+this["JST"]["index"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='';
+}
+return __p;
+};
+
+this["JST"]["item"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<a href="#'+
+( source.id )+
+'">\n  <img src="http://www.gravatar.com/avatar/'+
+( source.hash )+
+'"/>\n  <h3 class="contact-name">'+
+( source.name )+
+'</h3>\n  ';
+ if (source.phone) { 
+;__p+='<p>Phone: '+
+( source.phone )+
+'</p>';
+ } 
+;__p+='\n  ';
+ if (source.email) { 
+;__p+='<p>Email: '+
+( source.email )+
+'</p>';
+ } 
+;__p+='\n</a>\n';
+}
+return __p;
+};
---
+++ assets/js/views/mobile/ListView.js
@@ -0,0 +1,36 @@
+define([
+  'backbone',
+  './ItemView',
+  'jst/mobile'
+],
+function (Backbone, ItemView, JST) {
+
+  'use strict';
+
+  return Backbone.View.extend({
+    tagName: 'ul',
+    attributes: {
+      'data-role': 'listview',
+      'data-filter': 'true',
+      'data-filter-placeholder': 'Search'
+    },
+    // View methods
+    // ------------
+    render: function () {
+      var index;
+      this.$el.html('');
+      this.collection.each(function (contact) {
+        if (index !== contact.index()) {
+          index = contact.index();
+          // Insert list-divider
+          this.$el.append(JST.divider({index: index}));
+        }
+        var itemview = new ItemView({
+          model: contact
+        });
+        this.$el.append(itemview.render().el);
+      }, this);
+      return this;
+    }
+  });
+});

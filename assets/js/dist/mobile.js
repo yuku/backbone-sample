de7ab12f1a0225429396ff321cd10fab7a25637e
Yuku Takahashi <taka84u9@gmail.com>
2013-01-27 01:33:55 +0900

    build js

--- assets/js/dist/mobile.js
+++ assets/js/dist/mobile.js
@@ -24468,6 +24468,57 @@ define('fixtures',[],function () {
       github: "github"
     },
     {
+      name: "David Heinemeier Hansson",
+      email: "david@loudthinking.com",
+      github: "dhh",
+      twitter: "dhh"
+    },
+    {
+      name: "まつもと ゆきひろ",
+      email: "matz@ruby-lang.org",
+      github: "matz",
+      twitter: "yukihiro_matz"
+    },
+    {
+      name: "Guido van Rossum",
+      email: "guido@python.org",
+      github: "gvanrossum",
+      twitter: "gvanrossum"
+    },
+    {
+      name: "Tom Preston-Werner",
+      email: "tom@github.com",
+      github: "mojombo",
+      twitter: "mojombo"
+    },
+    {
+      name: "Chris Wanstrath",
+      email: "chris@github.com",
+      github: "defunkt",
+      twitter: "defunkt"
+    },
+    {
+      name: "PJ Hyett",
+      email: "pj@github.com",
+      github: "pjhyett",
+      twitter: "pjhyett"
+    },
+    {
+      name: "Mark Zuckerberg",
+      facebook: "zuck"
+    },
+    {
+      name: "Bill Gates",
+      twitter: "BillGates",
+      facebook: "BillGates"
+    },
+    {
+      name: "Jeremy Ashkenas",
+      email: "jashkenas@gmail.com",
+      github: "jashkenas",
+      twitter: "jashkenas"
+    },
+    {
       name: "Yuku Takahashi",
       email: "taka84u9@gmail.com",
       twitter: "taka84u9",

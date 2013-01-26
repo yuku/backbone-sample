84fc3948830c127f3c2b6dc8d4e16a43e0bcdce2
Yuku Takahashi <taka84u9@gmail.com>
2013-01-26 13:54:20 +0900

    Add jqm-config which configures jquery.mobile

---
+++ assets/js/jqm-config.js
@@ -0,0 +1,16 @@
+define(['jquery'], function () {
+
+  'use strict';
+
+  $(document).on('mobileinit', function () {
+    $.extend($.mobile, {
+      // Disable jQuery Mobile routing
+      ajaxEnabled: false,
+      linkBindingEnabled: false,
+      hashListeningEnabled: false,
+      pushStateEnabled: false,
+
+      defaultPageTransition: 'slide'
+    });
+  });
+});

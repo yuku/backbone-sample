22fc5e249bcf42149145da5ccbef2059f46237a2
Yuku Takahashi <taka84u9@gmail.com>
2013-01-27 12:44:54 +0900

    build

--- assets/js/dist/pc.js
+++ assets/js/dist/pc.js
@@ -14206,124 +14206,136 @@ define("backbone", ["jquery","underscore"], (function (global) {
 
 this["JST"] = this["JST"] || {};
 
-this["JST"]["pc/_form"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<form class="form-horizontal">\n  <div class="alert alert-error alert-block" style="display: none;">\n    <ul>\n    </ul>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="name">Name</label>\n    <div class="controls">\n      <input type="text" id="name" name="name"\n        value="'+
-( source.name || '' )+
-'" placeholder="Name" required/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="email">Email</label>\n    <div class="controls">\n      <input type="text" id="email" name="email"\n        value="'+
-( source.email || '' )+
-'" placeholder="Email"\n        pattern="[^\\s@]+@\\S+\\.\\S+"/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="phone">Phone</label>\n    <div class="controls">\n      <input type="text" id="phone" name="phone"\n        value="'+
-( source.phone || '' )+
-'" placeholder="Phone"/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="facebook">Facebook</label>\n    <div class="controls">\n      <input type="text" id="facebook" name="facebook"\n        value="'+
-( source.facebook || '' )+
-'" placeholder="Facebook"/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="twitter">Twitter</label>\n    <div class="controls">\n      <input type="text" id="twitter" name="twitter"\n        value="'+
-( source.twitter || '' )+
-'" placeholder="Twitter"/>\n    </div>\n  </div>\n  <div class="control-group">\n    <label class="control-label" for="github">GitHub</label>\n    <div class="controls">\n      <input type="text" id="github" name="github"\n        value="'+
-( source.github || '' )+
-'" placeholder="GitHub"/>\n    </div>\n  </div>\n  <div class="form-actions">\n    <button class="btn btn-primary submit">Submit</button>\n    <a href="#'+
-( source.id )+
-'" class="btn cancel">Cancel</a>\n    ';
- if (source.id) { 
-;__p+='\n      <span class="btn btn-danger delete">Delete</span>\n    ';
- } 
-;__p+='\n  </div>\n</form>\n';
+this["JST"]["pc/_form"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<form class="form-horizontal">\n<div class="alert alert-error alert-block" style="display: none;">\n<ul>\n</ul>\n</div>\n<div class="control-group">\n<label class="control-label" for="name">Name</label>\n<div class="controls">\n<input type="text" id="name" name="name"\nvalue="' +
+((__t = ( source.name || '' )) == null ? '' : __t) +
+'" placeholder="Name" required/>\n</div>\n</div>\n<div class="control-group">\n<label class="control-label" for="email">Email</label>\n<div class="controls">\n<input type="text" id="email" name="email"\nvalue="' +
+((__t = ( source.email || '' )) == null ? '' : __t) +
+'" placeholder="Email"\npattern="[^\\s@]+@\\S+\\.\\S+"/>\n</div>\n</div>\n<div class="control-group">\n<label class="control-label" for="phone">Phone</label>\n<div class="controls">\n<input type="text" id="phone" name="phone"\nvalue="' +
+((__t = ( source.phone || '' )) == null ? '' : __t) +
+'" placeholder="Phone"/>\n</div>\n</div>\n<div class="control-group">\n<label class="control-label" for="facebook">Facebook</label>\n<div class="controls">\n<input type="text" id="facebook" name="facebook"\nvalue="' +
+((__t = ( source.facebook || '' )) == null ? '' : __t) +
+'" placeholder="Facebook"/>\n</div>\n</div>\n<div class="control-group">\n<label class="control-label" for="twitter">Twitter</label>\n<div class="controls">\n<input type="text" id="twitter" name="twitter"\nvalue="' +
+((__t = ( source.twitter || '' )) == null ? '' : __t) +
+'" placeholder="Twitter"/>\n</div>\n</div>\n<div class="control-group">\n<label class="control-label" for="github">GitHub</label>\n<div class="controls">\n<input type="text" id="github" name="github"\nvalue="' +
+((__t = ( source.github || '' )) == null ? '' : __t) +
+'" placeholder="GitHub"/>\n</div>\n</div>\n<div class="form-actions">\n<button class="btn btn-primary submit">Submit</button>\n<a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'" class="btn cancel">Cancel</a>\n';
+ if (source.id) { ;
+__p += '\n<span class="btn btn-danger delete">Delete</span>\n';
+ } ;
+__p += '\n</div>\n</form>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["pc/app"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div id="header">\n  <h1>Contacts</h1>\n</div>\n<div id="content">\n  <div id="sidebar" class="clearfix">\n    <div id="sidebar-header">\n      <a href="#new" class="new">New Contact</a>\n    </div>\n    <div id="sidebar-content">\n      <div id="contactlist">\n      </div>\n    </div>\n  </div>\n  <div id="main">\n  </div>\n</div>\n';
-}
-return __p;
+this["JST"]["pc/app"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __d = obj.obj || obj;
+__p += '<div id="header">\n<h1>Contacts</h1>\n</div>\n<div id="content">\n<div id="sidebar" class="clearfix">\n<div id="sidebar-header">\n<a href="#new" class="new">New Contact</a>\n</div>\n<div id="sidebar-content">\n<div id="contactlist">\n</div>\n</div>\n</div>\n<div id="main">\n</div>\n</div>';
+return __p
 };
 
-this["JST"]["pc/edit"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+=''+
-( JST['pc/_form']({source: source}) )+
-'\n';
+this["JST"]["pc/edit"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p +=
+((__t = ( JST['pc/_form']({source: source}) )) == null ? '' : __t);
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["pc/item"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<a href="#'+
-( source.id )+
-'">\n  <img src="http://www.gravatar.com/avatar/'+
-( source.hash )+
-'"\n    class="thumbnail"/>\n  <h3>'+
-( source.name )+
-'</h3>\n  ';
- if (source.email) { 
-;__p+='<p>'+
-( source.email )+
+this["JST"]["pc/item"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'">\n<img src="http://www.gravatar.com/avatar/' +
+((__t = ( source.hash )) == null ? '' : __t) +
+'"\nclass="thumbnail"/>\n<h3>' +
+((__t = ( source.name )) == null ? '' : __t) +
+'</h3>\n';
+ if (source.email) { ;
+__p += '<p>' +
+((__t = ( source.email )) == null ? '' : __t) +
 '</p>';
- } 
-;__p+='\n  ';
- if (source.phone) { 
-;__p+='<p>'+
-( source.phone )+
+ } ;
+__p += '\n';
+ if (source.phone) { ;
+__p += '<p>' +
+((__t = ( source.phone )) == null ? '' : __t) +
 '</p>';
- } 
-;__p+='\n</a>\n';
+ } ;
+__p += '\n</a>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["pc/new"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+=''+
-( JST['pc/_form']({source: source}) )+
-'\n';
+this["JST"]["pc/new"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p +=
+((__t = ( JST['pc/_form']({source: source}) )) == null ? '' : __t);
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["pc/show"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<ul class="pager">\n  <li class="previous"><a href="#'+
-( source.id )+
-'/edit">Edit</a></li>\n</ul>\n<dl class="dl-horizontal">\n  <dt>Name</dt>\n  <dd>'+
-( source.name )+
-'</dd>\n  <dt>Email</dt>\n  <dd>'+
-( source.email || '&nbsp;' )+
-'</dd>\n  <dt>Phone</dt>\n  <dd>'+
-( source.phone || '&nbsp;' )+
-'</dd>\n  ';
- if (source.facebook) { 
-;__p+='\n    <dt>Facebook</dt>\n    <dd>\n      <a href="https://www.facebook.com/'+
-( source.facebook )+
-'"\n          target="_blank">\n        '+
-( source.facebook )+
-'\n      </a>\n    </dd>\n  ';
- } 
-;__p+='\n  ';
- if (source.twitter) { 
-;__p+='\n    <dt>Twitter</dt>\n    <dd>\n      <a href="https://twitter.com/'+
-( source.twitter )+
-'" target="_blank">\n        '+
-( source.twitter )+
-'\n      </a>\n    </dd>\n  ';
- } 
-;__p+='\n  ';
- if (source.github) { 
-;__p+='\n    <dt>Github</dt>\n    <dd>\n      <a href="https://github.com/'+
-( source.github )+
-'" target="_blank">\n        '+
-( source.github )+
-'\n      </a>\n    </dd>\n  ';
- } 
-;__p+='\n</dl>\n';
+this["JST"]["pc/show"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<ul class="pager">\n<li class="previous"><a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'/edit">Edit</a></li>\n</ul>\n<dl class="dl-horizontal">\n<dt>Name</dt>\n<dd>' +
+((__t = ( source.name )) == null ? '' : __t) +
+'</dd>\n<dt>Email</dt>\n<dd>' +
+((__t = ( source.email || '&nbsp;' )) == null ? '' : __t) +
+'</dd>\n<dt>Phone</dt>\n<dd>' +
+((__t = ( source.phone || '&nbsp;' )) == null ? '' : __t) +
+'</dd>\n';
+ if (source.facebook) { ;
+__p += '\n<dt>Facebook</dt>\n<dd>\n<a href="https://www.facebook.com/' +
+((__t = ( source.facebook )) == null ? '' : __t) +
+'"\ntarget="_blank">\n' +
+((__t = ( source.facebook )) == null ? '' : __t) +
+'\n</a>\n</dd>\n';
+ } ;
+__p += '\n';
+ if (source.twitter) { ;
+__p += '\n<dt>Twitter</dt>\n<dd>\n<a href="https://twitter.com/' +
+((__t = ( source.twitter )) == null ? '' : __t) +
+'" target="_blank">\n' +
+((__t = ( source.twitter )) == null ? '' : __t) +
+'\n</a>\n</dd>\n';
+ } ;
+__p += '\n';
+ if (source.github) { ;
+__p += '\n<dt>Github</dt>\n<dd>\n<a href="https://github.com/' +
+((__t = ( source.github )) == null ? '' : __t) +
+'" target="_blank">\n' +
+((__t = ( source.github )) == null ? '' : __t) +
+'\n</a>\n</dd>\n';
+ } ;
+__p += '\n</dl>';
+
 }
-return __p;
+return __p
 };
 define("jst/pc", (function (global) {
     return function () {
--- assets/js/jst/mobile.js
+++ assets/js/jst/mobile.js
@@ -1,141 +1,154 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["mobile/_form"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div data-role="fieldcontain">\n  <label for="contact_name">Name:</label>\n  <input type="text" name="name" id="contact_name"\n    value="'+
-( source.name || '' )+
-'" />\n  <div class="error" id="error_name"></div>\n</div>\n<div data-role="fieldcontain">\n  <label for="contact_phone">Phone:</label>\n  <input type="text" name="phone" id="contact_phone"\n    value="'+
-( source.phone || '' )+
-'" />\n  <div class="error" id="error_phone"></div>\n</div>\n<div data-role="fieldcontain">\n  <label for="contact_email">Email:</label>\n  <input type="text" name="email" id="contact_email"\n    value="'+
-( source.email || '' )+
-'" />\n  <div class="error" id="error_email"></div>\n</div>\n<div data-role="fieldcontain">\n  <label for="contact_twitter">Twitter:</label>\n  <input type="text" name="twitter" id="contact_twitter"\n    value="'+
-( source.twitter || '' )+
-'" />\n  <div class="error" id="error_twitter"></div>\n</div>\n<div data-role="fieldcontain">\n  <label for="contact_facebook">Github:</label>\n  <input type="text" name="github" id="contact_github"\n    value="'+
-( source.github || '' )+
-'" />\n  <div class="error" id="error_github"></div>\n</div>\n<div data-role="fieldcontain">\n  <label for="contact_facebook">Facebook:</label>\n  <input type="text" name="facebook" id="contact_facebook"\n    value="'+
-( source.facebook || '' )+
-'" />\n  <div class="error" id="error_facebook"></div>\n</div>\n';
+this["JST"]["mobile/_form"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<div data-role="fieldcontain">\n<label for="contact_name">Name:</label>\n<input type="text" name="name" id="contact_name"\nvalue="' +
+((__t = ( source.name || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_name"></div>\n</div>\n<div data-role="fieldcontain">\n<label for="contact_phone">Phone:</label>\n<input type="text" name="phone" id="contact_phone"\nvalue="' +
+((__t = ( source.phone || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_phone"></div>\n</div>\n<div data-role="fieldcontain">\n<label for="contact_email">Email:</label>\n<input type="text" name="email" id="contact_email"\nvalue="' +
+((__t = ( source.email || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_email"></div>\n</div>\n<div data-role="fieldcontain">\n<label for="contact_twitter">Twitter:</label>\n<input type="text" name="twitter" id="contact_twitter"\nvalue="' +
+((__t = ( source.twitter || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_twitter"></div>\n</div>\n<div data-role="fieldcontain">\n<label for="contact_facebook">Github:</label>\n<input type="text" name="github" id="contact_github"\nvalue="' +
+((__t = ( source.github || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_github"></div>\n</div>\n<div data-role="fieldcontain">\n<label for="contact_facebook">Facebook:</label>\n<input type="text" name="facebook" id="contact_facebook"\nvalue="' +
+((__t = ( source.facebook || '' )) == null ? '' : __t) +
+'" />\n<div class="error" id="error_facebook"></div>\n</div>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["mobile/divider"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<li data-role="list-divider">\n  '+
-( index )+
-'\n</li>\n';
-}
-return __p;
+this["JST"]["mobile/divider"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __d = obj.obj || obj;
+__p += '<li data-role="list-divider">\n' +
+((__t = ( obj.index )) == null ? '' : __t) +
+'\n</li>';
+return __p
 };
 
-this["JST"]["mobile/edit"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div data-role="header">\n  <h1>Edit</h1>\n</div>\n<div data-role="content">\n  <form action="#" method="POST">\n    '+
-( JST['mobile/_form']({source: source}) )+
-'\n    <div class="ui-body ui-body-c">\n      <fieldset class="ui-grid-a">\n        <div class="ui-block-a">\n          <a href="#'+
-( source.id )+
-'" data-role="button" data-theme="c">Cancel</a>\n        </div>\n        <div class="ui-block-b">\n          <button class="save" data-theme="b">Save</button>\n        </div>\n      </fieldset>\n      <hr/>\n      <span data-role="button" class="delete" data-theme="d">Delete</span>\n    </div>\n  </form>\n</div>\n';
+this["JST"]["mobile/edit"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<div data-role="header">\n<h1>Edit</h1>\n</div>\n<div data-role="content">\n<form action="#" method="POST">\n' +
+((__t = ( JST['mobile/_form']({source: source}) )) == null ? '' : __t) +
+'\n<div class="ui-body ui-body-c">\n<fieldset class="ui-grid-a">\n<div class="ui-block-a">\n<a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'" data-role="button" data-theme="c">Cancel</a>\n</div>\n<div class="ui-block-b">\n<button class="save" data-theme="b">Save</button>\n</div>\n</fieldset>\n<hr/>\n<span data-role="button" class="delete" data-theme="d">Delete</span>\n</div>\n</form>\n</div>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["mobile/index"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div data-role="header">\n  <h1>All Contacts</h1>\n  <a href="#new" class="ui-btn-right">New</a>\n</div>\n<div data-role="content">\n</div>\n';
-}
-return __p;
+this["JST"]["mobile/index"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __d = obj.obj || obj;
+__p += '<div data-role="header">\n<h1>All Contacts</h1>\n<a href="#new" class="ui-btn-right">New</a>\n</div>\n<div data-role="content">\n</div>';
+return __p
 };
 
-this["JST"]["mobile/item"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<a href="#'+
-( source.id )+
-'">\n  <img src="http://www.gravatar.com/avatar/'+
-( source.hash )+
-'"/>\n  <h3 class="contact-name">'+
-( source.name )+
-'</h3>\n  ';
- if (source.phone) { 
-;__p+='<p>Phone: '+
-( source.phone )+
+this["JST"]["mobile/item"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'">\n<img src="http://www.gravatar.com/avatar/' +
+((__t = ( source.hash )) == null ? '' : __t) +
+'"/>\n<h3 class="contact-name">' +
+((__t = ( source.name )) == null ? '' : __t) +
+'</h3>\n';
+ if (source.phone) { ;
+__p += '<p>Phone: ' +
+((__t = ( source.phone )) == null ? '' : __t) +
 '</p>';
- } 
-;__p+='\n  ';
- if (source.email) { 
-;__p+='<p>Email: '+
-( source.email )+
+ } ;
+__p += '\n';
+ if (source.email) { ;
+__p += '<p>Email: ' +
+((__t = ( source.email )) == null ? '' : __t) +
 '</p>';
- } 
-;__p+='\n</a>\n';
+ } ;
+__p += '\n</a>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["mobile/new"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div data-role="header">\n  <h1>New</h1>\n</div>\n<div data-role="content">\n  <form action="#" method="POST">\n    '+
-( JST['mobile/_form']({source: source}) )+
-'\n    <div class="ui-body ui-body-c">\n      <fieldset class="ui-grid-a">\n        <div class="ui-block-a">\n          <a href="#" data-role="button" data-theme="c">Cancel</a>\n        </div>\n        <div class="ui-block-b">\n          <button class="save" data-theme="b">Save</button>\n        </div>\n      </fieldset>\n    </div>\n  </form>\n</div>\n';
+this["JST"]["mobile/new"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<div data-role="header">\n<h1>New</h1>\n</div>\n<div data-role="content">\n<form action="#" method="POST">\n' +
+((__t = ( JST['mobile/_form']({source: source}) )) == null ? '' : __t) +
+'\n<div class="ui-body ui-body-c">\n<fieldset class="ui-grid-a">\n<div class="ui-block-a">\n<a href="#" data-role="button" data-theme="c">Cancel</a>\n</div>\n<div class="ui-block-b">\n<button class="save" data-theme="b">Save</button>\n</div>\n</fieldset>\n</div>\n</form>\n</div>';
+
 }
-return __p;
+return __p
 };
 
-this["JST"]["mobile/show"] = function(obj){
-var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
-with(obj||{}){
-__p+='<div data-role="header">\n  <a href="#" data-icon="back" class="back ui-btn-left">All Contacts</a>\n  <h1>Info</h1>\n  <a href="#'+
-( source.id )+
-'/edit" class="edit ui-btn-right">Edit</a>\n</div>\n<div data-role="content">\n  <div class="contact-head">\n    <div class="avatar">\n      <img src="http://www.gravatar.com/avatar/'+
-( source.hash )+
-'"/>\n    </div>\n    <div class="name">\n      <h3 data-name="name">'+
-( source.name )+
-'</h3>\n    </div>\n  </div>\n  <ul data-role="listview" data-inset="true">\n    ';
- if (source.phone) { 
-;__p+='\n      <li data-icon="false">\n        <a href="tel:'+
-( source.phone )+
-'">\n          <dl>\n            <dt>Phone</dt>\n            <dd>'+
-( source.phone || '' )+
-'</dd>\n          </dl>\n        </a>\n      </li>\n    ';
- } 
-;__p+='\n    ';
- if (source.email) { 
-;__p+='\n      <li data-icon="false" data-name="email">\n        <a href="mailto:'+
-( source.email )+
-'">\n          <dl>\n            <dt>Email</dt>\n            <dd>'+
-( source.email  || '' )+
-'</dd>\n          </dl>\n        </a>\n      </li>\n    ';
- } 
-;__p+='\n    ';
- if (source.twitter) { 
-;__p+='\n      <li data-icon="false" data-name="twitter">\n        <a href="https://twitter.com/'+
-( source.twitter )+
-'" target="_blank">\n          <dl>\n            <dt>Twitter</dt>\n            <dd>'+
-( source.twitter )+
-'</dd>\n          </dl>\n        </a>\n      </li>\n    ';
- } 
-;__p+='\n    ';
- if (source.github) { 
-;__p+='\n      <li data-icon="false" data-name="github">\n        <a href="https://github.com/'+
-( source.github )+
-'" target="_blank">\n          <dl>\n            <dt>GitHub</dt>\n            <dd>'+
-( source.github )+
-'</dd>\n          </dl>\n        </a>\n      </li>\n    ';
- } 
-;__p+='\n    ';
- if (source.facebook) { 
-;__p+='\n      <li data-icon="false" data-name="facebook">\n        <a href="https://www.facebook.com/'+
-( source.facebook )+
-'" target="_blank">\n          <dl>\n            <dt>Facebook</dt>\n            <dd>'+
-( source.facebook )+
-'</dd>\n          </dl>\n        </a>\n      </li>\n    ';
- } 
-;__p+='\n  </ul>\n</div>\n';
+this["JST"]["mobile/show"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<div data-role="header">\n<a href="#" data-icon="back" class="back ui-btn-left">All Contacts</a>\n<h1>Info</h1>\n<a href="#' +
+((__t = ( source.id )) == null ? '' : __t) +
+'/edit" class="edit ui-btn-right">Edit</a>\n</div>\n<div data-role="content">\n<div class="contact-head">\n<div class="avatar">\n<img src="http://www.gravatar.com/avatar/' +
+((__t = ( source.hash )) == null ? '' : __t) +
+'"/>\n</div>\n<div class="name">\n<h3 data-name="name">' +
+((__t = ( source.name )) == null ? '' : __t) +
+'</h3>\n</div>\n</div>\n<ul data-role="listview" data-inset="true">\n';
+ if (source.phone) { ;
+__p += '\n<li data-icon="false">\n<a href="tel:' +
+((__t = ( source.phone )) == null ? '' : __t) +
+'">\n<dl>\n<dt>Phone</dt>\n<dd>' +
+((__t = ( source.phone || '' )) == null ? '' : __t) +
+'</dd>\n</dl>\n</a>\n</li>\n';
+ } ;
+__p += '\n';
+ if (source.email) { ;
+__p += '\n<li data-icon="false" data-name="email">\n<a href="mailto:' +
+((__t = ( source.email )) == null ? '' : __t) +
+'">\n<dl>\n<dt>Email</dt>\n<dd>' +
+((__t = ( source.email  || '' )) == null ? '' : __t) +
+'</dd>\n</dl>\n</a>\n</li>\n';
+ } ;
+__p += '\n';
+ if (source.twitter) { ;
+__p += '\n<li data-icon="false" data-name="twitter">\n<a href="https://twitter.com/' +
+((__t = ( source.twitter )) == null ? '' : __t) +
+'" target="_blank">\n<dl>\n<dt>Twitter</dt>\n<dd>' +
+((__t = ( source.twitter )) == null ? '' : __t) +
+'</dd>\n</dl>\n</a>\n</li>\n';
+ } ;
+__p += '\n';
+ if (source.github) { ;
+__p += '\n<li data-icon="false" data-name="github">\n<a href="https://github.com/' +
+((__t = ( source.github )) == null ? '' : __t) +
+'" target="_blank">\n<dl>\n<dt>GitHub</dt>\n<dd>' +
+((__t = ( source.github )) == null ? '' : __t) +
+'</dd>\n</dl>\n</a>\n</li>\n';
+ } ;
+__p += '\n';
+ if (source.facebook) { ;
+__p += '\n<li data-icon="false" data-name="facebook">\n<a href="https://www.facebook.com/' +
+((__t = ( source.facebook )) == null ? '' : __t) +
+'" target="_blank">\n<dl>\n<dt>Facebook</dt>\n<dd>' +
+((__t = ( source.facebook )) == null ? '' : __t) +
+'</dd>\n</dl>\n</a>\n</li>\n';
+ } ;
+__p += '\n</ul>\n</div>';
+
 }
-return __p;
+return __p
 };
